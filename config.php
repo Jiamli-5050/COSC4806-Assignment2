da1809973bdb2f349daa9486d615a16be0cdcb67
Jiamli-5050 <jiamli@algomau.ca>
2025-05-31 01:58:16 +0000

    added some notes

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 define('DB_HOST',  'd53fw.h.filess.io');
 define('DB_USER',  'COSC4806_practical');
-define('DB_PASS',  $_ENV['DB_PASS']);
+define('DB_PASS',  $_ENV['DB_PASS']); //Secures the password so it will be hidden from the code
 define('DB_NAME',  'COSC4806_practical');
 define('DB_PORT',  '61000');
 
