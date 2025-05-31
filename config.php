cce703b28b803ea56e442f0f666ae024beb9c76f
Jiamli-5050 <jiamli@algomau.ca>
2025-05-31 01:25:23 +0000

    starting the structure

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DB_HOST',  'd53fw.h.filess.io');
+define('DB_USER',  'COSC4806_practical');
+define('DB_PASS',  $_ENV['DB_PASS']);
+define('DB_NAME',  'COSC4806_practical');
+define('DB_PORT',  '61000');
+
+?>
