8a622950645ebb1a7f659c400bd97e8b2839f570
Jiamli-5050 <jiamli@algomau.ca>
2025-05-31 01:44:21 +0000

    log in page to for users that are authenticated

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
+session_start();
+if (!($_SESSION['auth'] ?? false)) {
+    header('Location: login.php');
+    exit;
+}
+require_once('user.php');
+echo "<h1>Welcome, {$_SESSION['user']}!</h1>";
+echo "<p><a href='logout.php'>Log out</a></p>";
 
+$user = new User();
+echo "<pre>";
+print_r($user->get_all_users());
 ?>
