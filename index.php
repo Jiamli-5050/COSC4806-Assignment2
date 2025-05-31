f9fbd35c92ccfe8664f2873b3e24c3f30bc198d3
Jiamli-5050 <jiamli@algomau.ca>
2025-05-31 01:59:03 +0000

    added notes in understanding the function of the code

--- index.php
+++ index.php
@@ -3,12 +3,12 @@ session_start();
 if (!($_SESSION['auth'] ?? false)) {
     header('Location: login.php');
     exit;
-}
+} // if the user is not logged in, send them to the login page
 require_once('user.php');
 echo "<h1>Welcome, {$_SESSION['user']}!</h1>";
-echo "<p><a href='logout.php'>Log out</a></p>";
+echo "<p><a href='logout.php'>Log out</a></p>"; // let users log out
 
 $user = new User();
 echo "<pre>";
-print_r($user->get_all_users());
+print_r($user->get_all_users()); // print all users
 ?>
